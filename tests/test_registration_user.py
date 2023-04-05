2c478afb77f6e3d98cc8ebcc56a4268246da3fb6
Aleksandr Kozelskikh <a.n.kozelskikh@gmail.com>
2023-04-05 23:30:40 +0300

    HW 10.1.1
    Mid-level test case refactor name test and module

--- tests/test_registration_user.py
+++ tests/test_registration_user.py
@@ -1,7 +1,7 @@
 from automation_practice_form.model.pages.registration_page import RegistrationPage
 
 
-def test_hard():
+def test_registration_form():
     registration_page = RegistrationPage()
     registration_page.open()
     registration_page.fill_first_name('yashaka')
